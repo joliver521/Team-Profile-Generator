d012ae3029752562204e887b80a7d5527c196a5f
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-10 10:05:14 -0400

    add variables to index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquire = require('inquirer');
+const path = require('path');
+const fs = require('fs');
