15562bf1ea079592dfa47e3a5ccac9f3781f7b1e
Jason Oliver <58096660+joliver521@users.noreply.github.com>
2022-07-09 22:24:25 -0400

    Manager, Intern and Engineer tests pass

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+test('Set office number via constructor argument', () => {
+  const testValue = 100;
+  const e = new Manager('Foo', 1, 'test@test.com', testValue);
+  expect(e.officeNumber).toBe(testValue);
+});
+
+test('getRole() should return "Manager"', () => {
+  const testValue = 'Manager';
+  const e = new Manager('Foo', 1, 'test@test.com', 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test('Get office number via getOffice()', () => {
+  const testValue = 100;
+  const e = new Manager('Foo', 1, 'test@test.com', testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
